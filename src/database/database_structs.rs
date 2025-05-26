bfff8dff152841cc4d40d53ead59ab50b8f1f034
vaibhav <vaibhavmattoo1@gmail.com>
2025-05-26 15:22:21 -0500

    added some members

--- src/database/database_structs.rs
+++ src/database/database_structs.rs
@@ -1,19 +1,24 @@
 use std::collections::BTreeSet;
 
 #[derive(Debug)]
-struct Database {
+pub struct Database {
     command_list: BTreeSet<Command>,
     total_num_commands: i32,
     total_score: i64,
 }
 
 #[derive(Debug)]
-struct Suggestions {
-    alias_suggestion: String,
+pub struct deleted_commands {
+    deleted_commands: BTreeSet<Command>,
 }
 
 #[derive(Debug)]
-struct Command {
+pub struct Suggestions {
+    alias_suggestion: Vec<String>,
+}
+
+#[derive(Debug)]
+pub struct Command {
     last_access_time: i64,
     frequency: i32,
     length: i16,
